fe3359279c93ba36db77b42353a2b5205abc393a
Axel-curaudeau <axelcuraudeau@gmail.com>
2023-02-23 18:00:51 +0100

    commentaire + factorisation de code

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -23,23 +23,19 @@ public class player_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetKey(KeyCode.D) ? 1 : Input.GetKey(KeyCode.A) ? -1 : 0;
+        movement.y = Input.GetKey(KeyCode.W) ? 1 : Input.GetKey(KeyCode.S) ? -1 : 0;
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
-        
-        if (movement.x != 0 || movement.y != 0)
-        {
-            animator.SetBool("isRunning", true);
-        }
-        else
-        {
-            animator.SetBool("isRunning", false);
-        }
 
+        // Set the bool to true if we are moving in any position.
+        // The expression "movement.x != 0 || movement.y != 0" is a boolean expression that will be true
+        //      if either x or y is not 0. If both are 0, then the expression will be false.
+        animator.SetBool("isRunning", movement.x != 0 || movement.y != 0);
+        
         //flip
         if (movement.x > 0)
         {

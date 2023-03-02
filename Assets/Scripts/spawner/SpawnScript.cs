7127cbf9ca474f244cc1557a841bf37a6f8d09ac
Aurel <aurelien.dafonseca@gmail.com>
2023-03-02 11:10:33 +0000

    tir ennemi 2 et vague avec 2 types d ennemis

--- Assets/Scripts/spawner/SpawnScript.cs
+++ Assets/Scripts/spawner/SpawnScript.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 
 public class SpawnScript : MonoBehaviour
 {
-    public GameObject ennemi;
+    public GameObject[] ennemis= new GameObject[2];
     public GameObject[] spawnPoints = new GameObject[4];
     public int nombreEnnemi;
 
-    int randomInt;  //pour determiner le lieu de spawn
+    int randomPosition;  //pour determiner le lieu de spawn
+    int randomEnnemi;   //pour determiner l'enemi à spawn
     private bool isGenerating=true;
     public float waveTime;
     public float spawnTime;
@@ -25,10 +26,12 @@ public class SpawnScript : MonoBehaviour
         {
             for (int i = 0; i <= nombreEnnemi; i++)
             {
-                randomInt = UnityEngine.Random.Range(0, 4);
-                float varianceInt = Random.Range(-2f, 2f);  //pour espacer les ennemis au moment du spawn
-                Vector3 varianceVector = new Vector3(varianceInt, varianceInt, 0);
-                Instantiate(ennemi, spawnPoints[randomInt].transform.position+varianceVector, Quaternion.identity);
+                
+                randomEnnemi = Random.Range(0, 2);  //un ennemi aléatoire
+                randomPosition = Random.Range(0, 4);    //à une position aléatoire
+                //float varianceInt = Random.Range(-2f, 2f);  //pour espacer les ennemis au moment du spawn
+                //Vector3 varianceVector = new Vector3(varianceInt, varianceInt, 0);
+                Instantiate(ennemis[randomEnnemi], spawnPoints[randomPosition].transform.position, Quaternion.identity);
 
                 yield return new WaitForSeconds(spawnTime);
             }
